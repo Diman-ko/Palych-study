0b21433fd307e1b0ecd1682181a34734d9ba7c21
Diman-ko <dr.komarov@mail.ru>
2022-11-06 18:29:18 +0300

    Feat: add img_1.jpg

---
+++ src/script.js
@@ -0,0 +1,15 @@
+alert("Hello!");
+
+let nameYou = prompt('Wath is your name?');
+let nameF = prompt('Wath is  name your friend?');
+
+let appl = +prompt("How many apples do you have?");
+
+let appF = +prompt('How many apples does your friend have?');
+
+let sumAppl = appl + appF;
+
+let answer = `${nameYou} and ${nameF}  you have :   ${sumAppl}  apples!`;
+alert(answer);
+ console.log(answer);
+
